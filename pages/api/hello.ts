752559e8fea6d45f38e8f4274ea2e86932e56cce
if1live <libsora25@gmail.com>
2022-12-26 20:40:58 +0900

    external api test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const ts_start = Date.now();
+  const resp = await fetch("https://shiroko.fly.dev/execute_sql");
+  const json = await resp.json();
+  const ts_end = Date.now();
+  const millis = ts_end - ts_start;
+
+  res.status(200).json({
+    name: "John Doe",
+    millis,
+    x: import.meta.url,
+    ...json,
+  });
 }
