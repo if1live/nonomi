cd314d174b0be35bfd5f407385ace2043bf5697e
if1live <libsora25@gmail.com>
2022-12-26 20:52:08 +0900

    ytdl-core test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import ytdl, { videoFormat } from "ytdl-core";
 
 type Data = {
   name: string;
@@ -15,10 +16,27 @@ export default async function handler(
   const ts_end = Date.now();
   const millis = ts_end - ts_start;
 
+  const isAudioFormat = (format: videoFormat) =>
+    !format.width && !format.height;
+
+  const compareHighBitrate = (a: videoFormat, b: videoFormat) => {
+    const a_val = a.audioBitrate ?? -1;
+    const b_val = b.audioBitrate ?? -1;
+    return b_val - a_val;
+  };
+
+  const youtubeUrl = "https://www.youtube.com/watch?v=2WKBjmJCBHw";
+  const info = await ytdl.getInfo(youtubeUrl);
+  const formats = info.formats
+    .filter((x) => x.hasAudio && !x.hasVideo)
+    .filter(isAudioFormat)
+    .sort(compareHighBitrate);
+
   res.status(200).json({
     name: "John Doe",
     millis,
     x: import.meta.url,
+    formats,
     ...json,
   });
 }
