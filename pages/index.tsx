a4e06cde31f90670e484af2f55a2122d010eb225
if1live <libsora25@gmail.com>
2023-01-03 00:16:38 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,6 +59,8 @@ export default function Home() {
           </div>
         </div>
 
+        <div>TODO: a</div>
+
         <div className={styles.grid}>
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
