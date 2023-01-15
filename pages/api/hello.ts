919bc798fec92302d79a12f57f2d3a3abf5ea625
if1live <libsora25@gmail.com>
2023-01-15 23:02:31 +0900

    wip

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import ytdl, { videoFormat } from "ytdl-core";
 
 type Data = {
   name: string;
@@ -10,33 +9,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const ts_start = Date.now();
-  const resp = await fetch("https://shiroko.fly.dev/execute_sql");
-  const json = await resp.json();
-  const ts_end = Date.now();
-  const millis = ts_end - ts_start;
-
-  const isAudioFormat = (format: videoFormat) =>
-    !format.width && !format.height;
-
-  const compareHighBitrate = (a: videoFormat, b: videoFormat) => {
-    const a_val = a.audioBitrate ?? -1;
-    const b_val = b.audioBitrate ?? -1;
-    return b_val - a_val;
-  };
-
-  const youtubeUrl = "https://www.youtube.com/watch?v=2WKBjmJCBHw";
-  const info = await ytdl.getInfo(youtubeUrl);
-  const formats = info.formats
-    .filter((x) => x.hasAudio && !x.hasVideo)
-    .filter(isAudioFormat)
-    .sort(compareHighBitrate);
-
   res.status(200).json({
     name: "John Doe",
-    millis,
-    x: import.meta.url,
-    formats,
-    ...json,
   });
 }
